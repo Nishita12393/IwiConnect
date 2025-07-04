2249cb120e98ce8b232c6e833d9f4727918e1159
Nishita12393 <nishitathapa1111@gmail.com>
2025-07-04 10:08:21 +0545

    add user management module

--- usermgmt/urls.py
+++ usermgmt/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import user_list, view_citizenship_document
+from .views import user_list, view_citizenship_document, manage_iwi_leaders, manage_hapu_leaders
 
 urlpatterns = [
     path('users/', user_list, name='user_list'),
     path('view_document/<int:user_id>/', view_citizenship_document, name='view_citizenship_document'),
+    path('manage-iwi-leaders/', manage_iwi_leaders, name='manage_iwi_leaders'),
+    path('manage-hapu-leaders/', manage_hapu_leaders, name='manage_hapu_leaders'),
 ] 
--- usermgmt/views.py
+++ usermgmt/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import user_passes_test
-from core.models import CustomUser
+from django.contrib.auth.decorators import user_passes_test, login_required
+from core.models import CustomUser, Iwi, IwiLeader, Hapu, HapuLeader
 from django.urls import reverse
 from django.http import HttpResponseForbidden, FileResponse, Http404
 from django.conf import settings
@@ -42,3 +42,56 @@ def view_citizenship_document(request, user_id):
     if not os.path.exists(file_path):
         raise Http404()
     return FileResponse(open(file_path, 'rb'), as_attachment=False)
+
+@user_passes_test(lambda u: u.is_authenticated and u.is_staff)
+def manage_iwi_leaders(request):
+    iwis = Iwi.objects.all()
+    selected_iwi_id = request.GET.get('iwi')
+    selected_iwi = Iwi.objects.filter(id=selected_iwi_id).first() if selected_iwi_id else None
+    users = CustomUser.objects.filter(is_staff=False)
+    if selected_iwi:
+        users = users.filter(iwi=selected_iwi)
+    leaders = IwiLeader.objects.filter(iwi=selected_iwi) if selected_iwi else []
+    if request.method == 'POST' and selected_iwi:
+        add_user_id = request.POST.get('add_leader')
+        remove_user_id = request.POST.get('remove_leader')
+        if add_user_id:
+            user = CustomUser.objects.get(id=add_user_id)
+            IwiLeader.objects.get_or_create(iwi=selected_iwi, user=user)
+        if remove_user_id:
+            IwiLeader.objects.filter(iwi=selected_iwi, user_id=remove_user_id).delete()
+        return redirect(f'{request.path}?iwi={selected_iwi.id}')
+    return render(request, 'usermgmt/manage_iwi_leaders.html', {
+        'iwis': iwis,
+        'users': users,
+        'selected_iwi': selected_iwi,
+        'leaders': leaders,
+    })
+
+@login_required
+def manage_hapu_leaders(request):
+    # Only Iwi leaders can access
+    iwi_leaderships = IwiLeader.objects.filter(user=request.user)
+    iwis = [il.iwi for il in iwi_leaderships]
+    hapus = Hapu.objects.filter(iwi__in=iwis)
+    users = CustomUser.objects.filter(is_staff=False)
+    selected_hapu_id = request.GET.get('hapu')
+    selected_hapu = Hapu.objects.filter(id=selected_hapu_id, iwi__in=iwis).first() if selected_hapu_id else None
+    if selected_hapu:
+        users = users.filter(hapu=selected_hapu)
+    leaders = HapuLeader.objects.filter(hapu=selected_hapu) if selected_hapu else []
+    if request.method == 'POST' and selected_hapu:
+        add_user_id = request.POST.get('add_leader')
+        remove_user_id = request.POST.get('remove_leader')
+        if add_user_id:
+            user = CustomUser.objects.get(id=add_user_id)
+            HapuLeader.objects.get_or_create(hapu=selected_hapu, user=user)
+        if remove_user_id:
+            HapuLeader.objects.filter(hapu=selected_hapu, user_id=remove_user_id).delete()
+        return redirect(f'{request.path}?hapu={selected_hapu.id}')
+    return render(request, 'usermgmt/manage_hapu_leaders.html', {
+        'hapus': hapus,
+        'users': users,
+        'selected_hapu': selected_hapu,
+        'leaders': leaders,
+    })
